f5b2829c5a12f33706f5b9eab86d738322feb7dd
QingyangZhao <276676925@qq.com>
2018-01-22 16:14:47 -0800

    Vis Project

---
+++ data1.js
@@ -0,0 +1,578 @@
+var data1 = [
+ {
+   "y": 29,
+   "value": 9,
+   "x": 2,
+   "radius": 10
+ },
+ {
+   "y": 29,
+   "value": 5,
+   "x": 25,
+   "radius": 10
+ },
+ {
+   "y": 6,
+   "value": 38,
+   "x": 3,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 6,
+   "x": 6,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 7,
+   "x": 7,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 7,
+   "x": 8,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 22,
+   "x": 13,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 13,
+   "x": 21,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 15,
+   "x": 23,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 6,
+   "x": 25,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 14,
+   "x": 27,
+   "radius": 8
+ },
+ {
+   "y": 6,
+   "value": 14,
+   "x": 33,
+   "radius": 8
+ },
+ {
+   "y": 4,
+   "value": 4,
+   "x": 3,
+   "radius": 7
+ },
+ {
+   "y": 4,
+   "value": 5,
+   "x": 8,
+   "radius": 7
+ },
+ {
+   "y": 4,
+   "value": 5,
+   "x": 10,
+   "radius": 7
+ },
+ {
+   "y": 4,
+   "value": 3,
+   "x": 13,
+   "radius": 7
+ },
+ {
+   "y": 4,
+   "value": 3,
+   "x": 23,
+   "radius": 7
+ },
+ {
+   "y": 18,
+   "value": 18,
+   "x": 3,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 9,
+   "x": 7,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 24,
+   "x": 8,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 9,
+   "x": 9,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 7,
+   "x": 10,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 6,
+   "x": 14,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 5,
+   "x": 31,
+   "radius": 6.5
+ },
+ {
+   "y": 18,
+   "value": 14,
+   "x": 34,
+   "radius": 6.5
+ },
+ {
+   "y": 28,
+   "value": 5,
+   "x": 10,
+   "radius": 5
+ },
+ {
+   "y": 28,
+   "value": 3,
+   "x": 13,
+   "radius": 5
+ },
+ {
+   "y": 28,
+   "value": 7,
+   "x": 19,
+   "radius": 5
+ },
+ {
+   "y": 28,
+   "value": 3,
+   "x": 25,
+   "radius": 5
+ },
+ {
+   "y": 28,
+   "value": 4,
+   "x": 28,
+   "radius": 5
+ },
+ {
+   "y": 28,
+   "value": 2,
+   "x": 32,
+   "radius": 5
+ },
+ {
+   "y": 23,
+   "value": 5,
+   "x": 3,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 4,
+   "x": 6,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 12,
+   "x": 7,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 2,
+   "x": 9,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 8,
+   "x": 10,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 4,
+   "x": 17,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 4,
+   "x": 21,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 3,
+   "x": 22,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 6,
+   "x": 25,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 5,
+   "x": 27,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 10,
+   "x": 30,
+   "radius": 4
+ },
+ {
+   "y": 23,
+   "value": 7,
+   "x": 33,
+   "radius": 4
+ },
+ {
+   "y": 12,
+   "value": 6,
+   "x": 3,
+   "radius": 3
+ },
+ {
+   "y": 12,
+   "value": 3,
+   "x": 7,
+   "radius": 3
+ },
+ {
+   "y": 12,
+   "value": 9,
+   "x": 8,
+   "radius": 3
+ },
+ {
+   "y": 12,
+   "value": 8,
+   "x": 10,
+   "radius": 3
+ },
+ {
+   "y": 12,
+   "value": 3,
+   "x": 15,
+   "radius": 3
+ },
+ {
+   "y": 12,
+   "value": 4,
+   "x": 34,
+   "radius": 3
+ },
+ {
+   "y": 45,
+   "value": 10,
+   "x": 8,
+   "radius": 2.9
+ },
+ {
+   "y": 45,
+   "value": 5,
+   "x": 10,
+   "radius": 2.9
+ },
+ {
+   "y": 45,
+   "value": 4,
+   "x": 19,
+   "radius": 2.9
+ },
+ {
+   "y": 45,
+   "value": 2,
+   "x": 23,
+   "radius": 2.9
+ },
+ {
+   "y": 45,
+   "value": 2,
+   "x": 25,
+   "radius": 2.9
+ },
+ {
+   "y": 18,
+   "value": 18,
+   "x": 3,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 9,
+   "x": 7,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 24,
+   "x": 8,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 9,
+   "x": 9,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 7,
+   "x": 10,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 6,
+   "x": 14,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 5,
+   "x": 31,
+   "radius": 2.5
+ },
+ {
+   "y": 18,
+   "value": 14,
+   "x": 34,
+   "radius": 2.5
+ },
+ {
+   "y": 37,
+   "value": 4,
+   "x": 1,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 9,
+   "x": 5,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 5,
+   "x": 6,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 6,
+   "x": 7,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 9,
+   "x": 11,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 3,
+   "x": 14,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 3,
+   "x": 25,
+   "radius": 2
+ },
+ {
+   "y": 37,
+   "value": 4,
+   "x": 35,
+   "radius": 2
+ },
+ {
+   "y": 7,
+   "value": 2,
+   "x": 2,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 33,
+   "x": 3,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 32,
+   "x": 4,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 6,
+   "x": 7,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 20,
+   "x": 8,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 5,
+   "x": 9,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 13,
+   "x": 10,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 16,
+   "x": 13,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 6,
+   "x": 13,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 2,
+   "x": 14,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 20,
+   "x": 14,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 4,
+   "x": 15,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 20,
+   "x": 15,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 11,
+   "x": 21,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 5,
+   "x": 23,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 7,
+   "x": 23,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 16,
+   "x": 24,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 2,
+   "x": 25,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 5,
+   "x": 27,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 3,
+   "x": 28,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 6,
+   "x": 28,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 8,
+   "x": 32,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 6,
+   "x": 33,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 5,
+   "x": 33,
+   "radius": 1
+ },
+ {
+   "y": 7,
+   "value": 7,
+   "x": 34,
+   "radius": 1
+ },
+ {
+   "y": 44,
+   "value": 6,
+   "x": 34,
+   "radius": 1
+ }
+]
---
+++ datalink1.js
@@ -0,0 +1,17 @@
+var datalink1 = [
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ],
+  ["http//news.163.com/17/0324/15/CGA6V5KL0001875P.html#f=post1603_tab_news"  ]
+]
